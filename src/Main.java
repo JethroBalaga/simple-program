ff90357b7ee5199c2832f3e30035b30a9703b286
JethroBalaga <balagajet@gmail.com>
2023-10-09 07:40:01 +0800

    added initial files

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author PC
+ */
+public class Main {
+    public static void main(String[]args){
+        System.out.println("hello world");//pinka basic sa programming
+    }
+}
